10549278fa92cb7c7d399ebcd4ac6c58f012b631
Michael Noel <63615275+micka190@users.noreply.github.com>
2020-09-01 21:35:31 -0600

    Update ps-profile.ps1

--- files/ps-profile.ps1
+++ files/ps-profile.ps1
@@ -2,9 +2,17 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Powerlevel10k-Lean
 
-# Get Git's `ls` output.
 if ($host.Name -eq 'ConsoleHost')
 {
-    function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
-    Set-Alias -Name ls -Value ls_git -Option Private
+        # Enable unix-like `ls` behavior by literally using Git's packaged `ls` command.
+        function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
+        Set-Alias -Name ls -Value ls_git -Option Private
+
+        # Superclear
+        function superclear { clear;Get-Location | ForEach-Object {$_.Path} ;ls_git -A; }
+        Set-Alias -Name c -Value superclear -Option Private
+
+        # Edit $PROFILE file
+        function edit_profile { nvim $PROFILE }
+        Set-Alias -Name prc -Value edit_profile -Option Private
 }
