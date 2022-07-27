2823a8bf002d742e38cf0cc8535773d065a3f36f
Michael <63615275+micka190@users.noreply.github.com>
2022-07-26 19:56:59 -0600

    Cleared repo. Doing a full rewrite.

--- scripts/Shared.ps1
+++
@@ -1,5 +0,0 @@
-function Write-AlreadyInstalled($ModuleName)
-{
-  Write-Host $ModuleName -ForegroundColor Cyan -NoNewline
-  Write-Host " is already installed. Skipping installation..." -ForegroundColor Green
-}
